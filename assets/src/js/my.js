000b937a2d7d0a19353b3d87b069705f6d898075
Andrey Chepulis <andrey.chepulis@kreo.net>
2019-12-06 23:34:05 +0300

    start 3 pages

---
+++ assets/src/js/my.js
@@ -0,0 +1,19 @@
+var url_string = window.location.href;
+function findActivePage (name) {
+  if (name) {
+    var id = '#' + name;
+    $(id).addClass('active');
+  } else {
+    $('#home').addClass('active');
+  }
+}
+
+if (url_string.indexOf("products") !== -1) {
+  findActivePage("products");
+} else if (url_string.indexOf("projects") !== -1) {
+  findActivePage("projects");
+} else {
+  findActivePage();
+}
+
+// console.log(url_string, findActivePage("products"));
